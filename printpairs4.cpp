422954f439842b25d78a784b47384dbe89fa8263
K24-Code <kashishvijay2405@gmail.com>
2025-03-16 21:45:19 +0530

    Add files via upload

---
+++ printpairs4.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+void printAllPairs(int arr[], int n)
+{
+    for(int i = 0; i<n; i++)
+    {
+        //f=FOR EVERY i, WE ARE RUNNING LOOPING VARIABLE j FROM START TO END
+        for(int j=0; j<i; j++){
+            cout<< arr[i] << "," << arr[j] <<endl;
+        }
+    }
+}
+int main()
+{
+    int arr[] = {10, 20, 30, 40};
+    int n=4;
+    printAllPairs(arr,n);
+    return 0;
+}
